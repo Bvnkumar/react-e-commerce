284dce0ba313d5d424f99d729497a307921edd17
pvungarala1 <pvungarala1@web.com>
2019-08-23 19:01:55 +0530

    code changes related to validation

--- website/src/components/signup/signup.js
+++ website/src/components/signup/signup.js
@@ -1,31 +1,47 @@
 import React from 'react';
 import './signup.scss';
-
+let errorStyle ={
+    color:"#d9534f"
+};
 class SignupComponent extends React.Component {
     constructor() {
         super()
-        this.state = { firstname: '', lastname: '', email: '', password: '' };
-        this.onEmailChange = this.onEmailChange.bind(this);
-        this.onPassChange = this.onPassChange.bind(this);
-        this.onFirstNameChange = this.onFirstNameChange.bind(this);
-        this.onLastNameChange = this.onLastNameChange.bind(this);
+        this.state = { firstname: '', lastname: '', email: '', password: '',errors: {} };
         this.submitSignup = this.submitSignup.bind(this);
         this.goToLogin = this.goToLogin.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
 
     }
-    onEmailChange(event) {
-        this.setState({ email: event.target.value });
-    }
-    onPassChange(event) {
-        this.setState({ password: event.target.value });
-    }
-    onFirstNameChange(event) {
-        this.setState({ firstname: event.target.value });
-    }
-    onLastNameChange(event) {
-        this.setState({ lastname: event.target.value });
+
+    handleInputChange(name, value){
+        let errors = this.state.errors;
+        if( !value ){
+            errors[name] = "This field is required!!" ;
+         }
+         else {
+             delete errors[name];
+         }
+        this.setState({[name] : value });
+        this.setState({errors});
     }
     submitSignup(event) {
+        event.preventDefault();
+        const required  = "This field is required!";
+        let errors = this.state.errors;
+        if(!this.state.firstname){
+            errors.firstname = required;
+         }
+        if(!this.state.lastname){
+           errors.lastname = required;
+        }
+        if(!this.state.email){
+            errors.email = required;
+        }
+        if(!this.state.password){
+            errors.password = required;
+        }
+        this.setState({errors});
+        console.log("this.state",this.state);
         fetch('http://localhost:3001/auth/signup', { method: "post", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(this.state) }).
             then((res) => console.log("res.json() ", res.json())
             )
@@ -43,7 +59,8 @@ class SignupComponent extends React.Component {
                         <div className="col-8 offset-2">
                             <div className="form-group">
                                 <label className="form-label">Firstname:</label>
-                                <input type="text" className="form-control" onChange={this.onFirstNameChange}></input>
+                                <input type="text" className="form-control" onChange={(e)=> this.handleInputChange("firstname", e.target.value)}></input>
+                                <span>{this.state.errors.firstname ? <span style={errorStyle}>{this.state.errors.firstname}</span> : ""}</span>
                             </div>
                         </div>
                     </div>
@@ -51,7 +68,8 @@ class SignupComponent extends React.Component {
                         <div className="col-8 offset-2">
                             <div className="form-group">
                                 <label className="form-label">Lastname:</label>
-                                <input type="text" className="form-control" onChange={this.onLastNameChange}></input>
+                                <input type="text" className="form-control" onChange={(e)=> this.handleInputChange("lastname", e.target.value)}></input>
+                                <span>{this.state.errors.lastname ? <span style={errorStyle}>{this.state.errors.lastname}</span> : ""}</span>
                             </div>
                         </div>
                     </div>
@@ -59,7 +77,8 @@ class SignupComponent extends React.Component {
                         <div className="col-8 offset-2">
                             <div className="form-group">
                                 <label className="form-label">Username:</label>
-                                <input type="text" className="form-control" onChange={this.onEmailChange}></input>
+                                <input type="text" className="form-control" onChange={(e)=> this.handleInputChange("email", e.target.value)}></input>
+                                <span>{this.state.errors.email ? <span style={errorStyle}>{this.state.errors.email}</span> : ""}</span>
                             </div>
                         </div>
                     </div>
@@ -67,7 +86,8 @@ class SignupComponent extends React.Component {
                         <div className="col-8 offset-2">
                             <div className="form-group">
                                 <label className="form-label">Password:</label>
-                                <input type="password" className="form-control" onChange={this.onPassChange}></input>
+                                <input type="password" className="form-control" onChange={(e)=> this.handleInputChange("password", e.target.value)}></input>
+                                <span>{this.state.errors.password ? <span style={errorStyle}>{this.state.errors.password}</span> : ""}</span>
                             </div>
                         </div>
                     </div>
